9d3b53f610bb9803685b2e6289638eac6881e01c
thebestfantasy <sergej.fronch@gmail.com>
2023-05-22 18:48:14 +0000

    Deploying to gh-pages from @ thebestfantasy/goit-js-hw-11@9207d683738e8b4cbfbe9ec5f91d42cd908cf433 🚀

---
+++ index.5e49c9ad.js
@@ -0,0 +1,2 @@
+!function(){var e={method:"GET",headers:{Authorization:"Bearer ".concat("345007f9ab440e5b86cef51be6397df1")}};fetch("".concat("https://api.themoviedb.org/3").concat("/trending/movie/week"),e).then((function(e){return console.log(e)}))}();
+//# sourceMappingURL=index.5e49c9ad.js.map
