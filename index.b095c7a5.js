e887ea3d1e5fbc431bffa0e9e493b7312deb74bb
thebestfantasy <sergej.fronch@gmail.com>
2023-06-04 07:08:17 +0000

    Deploying to gh-pages from @ thebestfantasy/goit-js-hw-11@0f76bc6a98b633559896d55ccc5b4ba4e68d3a05 🚀

--- index.b095c7a5.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.themoviedb.org/3/trending/movie/week",{method:"GET",headers:{Authorization:"Bearer 345007f9ab440e5b86cef51be6397df1"}}).then((e=>console.log(e)));
-//# sourceMappingURL=index.b095c7a5.js.map
